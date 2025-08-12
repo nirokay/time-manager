58a974e782b4934bdfc5728e2ff718f5a6be18a7
nirokay <nirokay-public@protonmail.com>
2025-08-12 11:12:36 +0200

    parsing of data to object, not yet tested

--- src/database.nim
+++ src/database.nim
@@ -1,12 +1,59 @@
-import std/[asynchttpserver, asyncdispatch, strutils]
+import std/[asynchttpserver, asyncdispatch, strutils, uri, base64]
 import db_connector/db_sqlite
 import cattag
 import typedefs, htmlstuff
 
 const dbName {.strdefine.}: string = "database.db"
 
+proc getDatabase(): DbConn = open(dbName, "", "", "")
+template withDatabase*(db: untyped, body: untyped): untyped =
+    ## Template to avoid writing repetitive code
+    let db: DbConn = getDatabase()
+    db.exec(sql"BEGIN TRANSACTION")
+    var success: bool = true
+    try:
+        body
+        success = true
+    except CatchableError as e:
+        success = false
+    finally:
+        if success: db.exec(sql"COMMIT")
+        db.close()
+
+
 proc handlePayloadSubmission*(payload: string): ServerResponse =
     if payload == "": return ServerResponse(
         code: Http400,
         content: htmlPageFailure(p(html"Empty payload."))
+    )
+    let
+        decodedUri: string = block:
+            try:
+                payload.decodeUrl()
+            except CatchableError:
+                ""
+        decoded: string = block:
+            try:
+                decodedUri.decode()
+            except ValueError:
+                ""
+    if unlikely decoded == "": return ServerResponse(
+        code: Http400,
+        content: htmlPageFailure(
+            p(html"Malformed payload."),
+            p(
+                html"Decoded from URI:",
+                if decodedUri == "": i(html"empty") else: pre(html decodedUri)
+            ),
+            p(
+                html"Decoded from Base64:",
+                if decoded == "": i(html"empty") else: pre(html decoded)
+            )
+        )
+    )
+
+    let parseResult: Result[UserInput] = parseUserInput(payload)
+    if parseResult.error: return ServerResponse(
+        code: Http400,
+        content: htmlPageFailure()
     )
--- src/typedefs.nim
+++ src/typedefs.nim
@@ -1,5 +1,93 @@
-import std/[asynchttpserver, asyncdispatch]
+import std/[asynchttpserver, asyncdispatch, strformat, tables, json, options, re]
 type
     ServerResponse* = object
         code*: HttpCode
         content*: string
+
+    Days* = enum
+        Monday
+        Tuesday
+        Wednesday
+        Thursday
+        Friday
+        Saturday
+        Sunday
+
+    UserInput* = object
+        username*: string
+        timezone*: int
+        times*: OrderedTable[Days, array[2, string]]
+
+    Result*[T] = object
+        error*: bool
+        message*: string
+        result*: Option[T]
+
+proc fail(message: string): Result[UserInput] = Result[UserInput](
+    error: true,
+    message: message,
+    result: none UserInput
+)
+
+const timeRegex: Regex = re"^\D\D:\D\D"
+proc parseUserInput*(json: JsonNode): Result[UserInput] = # what a mf does for good error messages...
+    if not json.fields.hasKey("username"): return fail("Field <pre>username</pre> does not exist.")
+    if not json.fields.hasKey("timezone"): return fail("Field <pre>timezone</pre> does not exist.")
+    if not json.fields.hasKey("times"): return fail("Field <pre>times</pre> does not exist.")
+
+    let
+        rawUsername: JsonNode = json.fields["username"]
+        rawTimezone: JsonNode = json.fields["timezone"]
+        rawTimes: JsonNode = json.fields["times"]
+
+    if rawUsername.kind != JString: return fail("Field <pre>username</pre> is not of type <pre>string</pre>.")
+    if rawTimezone.kind != JInt: return fail("Field <pre>timezone</pre> is not of type <pre>int</pre>.")
+    if rawTimes.kind != JObject: return fail("Field <pre>times</pre> is not of type <pre>object</pre>.")
+
+    let
+        username: string = rawUsername.str
+        timezone: int = rawTimezone.num
+        timesFields: OrderedTable[string, JsonNode] = rawTimes.fields
+    var userInput: UserInput = UserInput(
+        username: username,
+        timezone: timezone
+    )
+
+    for dayEnum in Days:
+        let day: string = $dayEnum
+        if not timesFields.hasKey(day): return fail(&"Missing data for day {day}.") # should not happen with normal usage
+
+        let node: JsonNode = timesFields[day]
+        if node.kind != JArray: return fail(&"Malformed data for day {day}, is not of type <pre>array</pre>.")
+
+        let list: seq[JsonNode] = node.elems
+        if list.len() != 2: return fail(&"Malformed data for day {day}, array has a length of {list.len()} instead of 2.")
+
+        for index, value in list:
+            if value.kind != JString: return fail(&"Index {index} in list for day {day} is not of type <pre>string</pre>.")
+
+        let
+            timeStart: string = list[0].str
+            timeEnd: string = list[1].str
+        if (timeStart == "" and timeEnd != "") or (timeStart != "" and timeEnd == ""):
+            return fail(&"Malformed data for day {day}, both array values must be either set or empty <pre>string</pre>s!")
+
+        for i, time in [timeStart, timeEnd]:
+            if time == "": continue
+            if not match(time, timeRegex):
+                return fail(&"Malformed data for day {day} in index {i}, time <pre>{time}</pre> does not match regex.")
+
+        userInput.times[dayEnum] = [timeStart, timeEnd]
+
+    return Result[UserInput](
+        error: false,
+        message: "",
+        result: some userInput
+    )
+
+proc parseUserInput*(payload: string): Result[UserInput] =
+    try:
+        let json: JsonNode = payload.parseJson()
+        result = json.parseUserInput()
+    except ValueError:
+        return fail("Unparsable JSON.")
